b2e012c056efdb64a1a878eee540a3112084d6b1
amr3ita <amrwalied90@gmail.com>
2021-12-21 13:11:29 +0200

    data structure

--- LinkedList/src/linkedlist.java
+++ LinkedList/src/linkedlist.java
@@ -6,20 +6,52 @@ class Node {
 
 public class linkedlist {
     Node first;
+    Node last;
 
     public boolean isEmpty() {
         return (first == null);
     }
 
-    public void insertfirst(int v) {
+    public void insertfirst(int value) {
         Node newnode = new Node();
-        newnode.data = v;
+        newnode.data = value;
+        if (first == null) {
+            last = newnode;
+        }
         newnode.next = first;
         first = newnode;
     }
 
     public void deletefirst() {
+        if (first == null) {
+            return;
+        } else if (first.next == null) {
+            last = null;
+        }
         first = first.next;
+    }
+
+    public void insertlast(int value) {
+        Node newnode = new Node();
+        newnode.data = value;
+        if (first == null) {
+            first = newnode;
+        } else {
+            last.next = newnode;
+        }
+        last = newnode;
+    }
+
+    public void deletelast() {
+        if (first == null)
+            return;
+        if (first == last)
+            first = last = null;
+        Node current = first;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
     }
 
     public Node search(int key) {
@@ -33,27 +65,46 @@ public class linkedlist {
         return null;
     }
 
-    public Node deleteitem(int key) {
-        Node current = first;
-        Node previous = first;
+    public void insert(int pos, int value) {
+        Node newnode = new Node();
+        newnode.data = value;
 
-        while (current.data != key) {
-            if (current.next == null) {
-                return null;
-            }
-            previous = current;
+        Node current = first;
+        for (int i = 1; i < pos - 1; i++) {
             current = current.next;
         }
-
-        if (current == first) {
-            first = first.next;
+        if (pos == 1) {
+            newnode.next = first;
+            first = newnode;
+        } else if (current == first) {
+            newnode.next = current.next;
+            current.next = newnode;
+        } else if (current == last) {
+            current.next = newnode;
+            last = newnode;
         } else {
-            previous.next = current.next;
+            newnode.next = current.next;
+            current.next = newnode;
         }
-        return current;
     }
 
-    public void print() {
+    public void deleteitem(int key) {
+        Node current = first;
+        Node prev = first;
+        while (current.data != key) {
+            prev = current;
+            current = current.next;
+        }
+        if (current == first) {
+            deletefirst();
+        } else if (current == last) {
+            deletelast();
+        } else {
+            prev.next = current.next;
+        }
+    }
+
+    public void display() {
         Node current = first;
         while (current != null) {
             System.out.print(current.data + " ");
@@ -64,15 +115,12 @@ public class linkedlist {
 
     public static void main(String[] args) {
         linkedlist li = new linkedlist();
-        li.insertfirst(5);
-        li.insertfirst(7);
-        li.insertfirst(6);
-        li.insertfirst(9);
-        li.print();
-        System.out.println(li.search(7));
-        li.deletefirst();
-        li.print();
-        li.deleteitem(6);
-        li.print();
+
+        li.insertlast(5);
+        li.insertlast(6);
+        li.insertlast(9);
+        li.insert(3, 4);
+
+        li.display();
     }
 }
